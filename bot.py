27359fb9c5c964cf36f34bc91a3df18161236349
mohammedamine31 <93851884+mohammedamine31@users.noreply.github.com>
2026-04-20 20:30:10 +0100

    bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,8 @@ def home():
     return "Bot is running ✅"
 
 def run_web():
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run_web)
